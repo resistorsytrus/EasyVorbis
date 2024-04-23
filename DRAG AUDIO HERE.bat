612c89528a6dd6657e115a742da739072d007815
resistorsytrus <129260667+resistorsytrus@users.noreply.github.com>
2024-04-23 23:20:09 +0900

    Update DRAG AUDIO HERE.bat
    Fixed lowpass filter

--- DRAG AUDIO HERE.bat
+++ DRAG AUDIO HERE.bat
@@ -1,4 +1,4 @@
 @echo off
-%~dp0ffmpeg.exe -hide_banner -i %1 -af aresample=32000:resampler=soxr:dither_scale=1:dither_method=improved_e_weighted:precision=28:cutoff=1 -vn -c:a pcm_f32le -f f32le - | %~dp0oggenc2.exe -r -F 3 -B 32 -R 32000 -q 0.571429 --advanced-encode-option impulse_noisetune=0 --advanced-encode-option impulse_trigger_profile=6 -o %1.ogg -
+%~dp0ffmpeg.exe -hide_banner -i %1 -af aresample=32000:resampler=soxr:dither_scale=1:dither_method=improved_e_weighted:precision=28:cutoff=1 -vn -c:a pcm_f32le -f f32le - | %~dp0oggenc2.exe -r -F 3 -B 32 -R 32000 -q 0.571429 --advanced-encode-option impulse_noisetune=0 --advanced-encode-option impulse_trigger_profile=6 --advanced-encode-option lowpass_frequency=99 -o %1.ogg -
 %~dp0optivorbis.exe %1.ogg %~n1.ogg
 del /Q %1.ogg
