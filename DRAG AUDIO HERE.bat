3a00a03c35a4b6ee5db4567fc3e5753a5161cd54
resistorsytrus <129260667+resistorsytrus@users.noreply.github.com>
2024-04-23 23:11:13 +0900

    Update DRAG AUDIO HERE.bat
    Change to oggenc2 from venc

--- DRAG AUDIO HERE.bat
+++ DRAG AUDIO HERE.bat
@@ -1,6 +1,4 @@
 @echo off
-%~dp0ffmpeg.exe -hide_banner -i %1 -af aresample=32000:resampler=soxr:dither_scale=1:dither_method=improved_e_weighted:precision=28:cutoff=1 -vn -c:a pcm_f32le %1.wav
-%~dp0venc.exe -q0.571429 -lowpass=99 -ibtune=-15 %1.wav
-del /Q %1.wav
+%~dp0ffmpeg.exe -hide_banner -i %1 -af aresample=32000:resampler=soxr:dither_scale=1:dither_method=improved_e_weighted:precision=28:cutoff=1 -vn -c:a pcm_f32le -f f32le - | %~dp0oggenc2.exe -r -F 3 -B 32 -R 32000 -q 0.571429 --advanced-encode-option impulse_noisetune=0 --advanced-encode-option impulse_trigger_profile=6 -o %1.ogg -
 %~dp0optivorbis.exe %1.ogg %~n1.ogg
 del /Q %1.ogg
